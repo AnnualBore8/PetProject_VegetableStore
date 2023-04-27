6b1fb888bae780b1492b39ed5e261edd0621c846
Egor <egorobidin@inbox.ru>
2023-04-27 16:40:41 +0300

    Не работает, нужно доделать

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Product, Discount
+from django.db.models import OuterRef, Subquery, F, ExpressionWrapper, DecimalField
+from django.db.models.functions import Round, Cast
+from decimal import Decimal
 
 class ProductView(View):
 
@@ -11,57 +14,13 @@ class ProductView(View):
 class ShopView(View):
 
     def get(self, request):
-        context = {'data': [{'name': 'Bell Pepper',
-                             'discount': 30,
-                             'price_before': 120.00,
-                             'price_after': 80.00,
-                             'url': 'store/images/product-1.jpg'},
-                            {'name': 'Strawberry',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-2.jpg'},
-                            {'name': 'Green Beans',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-3.jpg'},
-                            {'name': 'Purple Cabbage',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-4.jpg'},
-                            {'name': 'Tomatoe',
-                             'discount': 30,
-                             'price_before': 120.00,
-                             'price_after': 80.00,
-                             'url': 'store/images/product-5.jpg'},
-                            {'name': 'Brocolli',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-6.jpg'},
-                            {'name': 'Carrots',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-7.jpg'},
-                            {'name': 'Fruit Juice',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-8.jpg'},
-                            {'name': 'Onion',
-                             'discount': 30,
-                             'price_before': 120.00,
-                             'price_after': 80.00,
-                             'url': 'store/images/product-9.jpg'},
-                            {'name': 'Apple',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-10.jpg'},
-                            {'name': 'Garlic',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-11.jpg'},
-                            {'name': 'Chilli',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'url': 'store/images/product-12.jpg'},
-                            ]
-                   }
-        return render(request, 'shop/shop.html', context)
+        products = Product.objects.annotate(
+            discount_value=Subquery(
+                Discount.objects.filter(product_id=OuterRef('id')).values('value')
+            ),
+            price_before=F('price'),
+            price_after=ExpressionWrapper(Round(F('price_before') * (100.0 - F('discount_value')) / 101.0, 2),
+                                          output_field=DecimalField(max_digits=10, decimal_places=2)
+                                          )
+        ).values('id', 'name', 'image', 'price_before', 'price_after', 'discount_value') # TODO доделать
+        return render(request, 'shop/shop.html', {"data": products})
