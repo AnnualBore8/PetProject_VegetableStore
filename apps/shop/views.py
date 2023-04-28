ffb0bb1b07aaa359e385b7d6cc39f3e374731b52
Egor <egorobidin@inbox.ru>
2023-04-28 16:12:01 +0300

    владка shop отображает товары с БД

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -14,13 +14,17 @@ class ProductView(View):
 class ShopView(View):
 
     def get(self, request):
+
+        price_with_discount = ExpressionWrapper(
+            F('price') * (100.0 - F('discount_value')) / 100.0,
+            output_field=DecimalField(max_digits=10, decimal_places=2)
+        )
+
         products = Product.objects.annotate(
             discount_value=Subquery(
                 Discount.objects.filter(product_id=OuterRef('id')).values('value')
             ),
             price_before=F('price'),
-            price_after=ExpressionWrapper(Round(F('price_before') * (100.0 - F('discount_value')) / 101.0, 2),
-                                          output_field=DecimalField(max_digits=10, decimal_places=2)
-                                          )
-        ).values('id', 'name', 'image', 'price_before', 'price_after', 'discount_value') # TODO доделать
+            price_after=price_with_discount
+        ).values('id', 'name', 'image', 'price_before', 'price_after', 'discount_value')
         return render(request, 'shop/shop.html', {"data": products})
