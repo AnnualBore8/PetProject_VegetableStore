968a4798757bbb52097442aeecd5305537567027
Egor <egorobidin@inbox.ru>
2023-05-01 20:58:56 +0300

    Добавили переход к продукту и выбор категории в shop

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -5,5 +5,6 @@ app_name = "shop"
 
 urlpatterns = [
    path('', ShopView.as_view(), name="shop"),
+   path('product/<int:product_id>', ProductView.as_view(), name="product-single"),
    path('product/', ProductView.as_view(), name="product-single"),
 ]
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.views import View
 from .models import Product, Discount
 from django.db.models import OuterRef, Subquery, F, ExpressionWrapper, DecimalField
+from django.shortcuts import get_object_or_404
 from django.db.models.functions import Round, Cast
 from decimal import Decimal
 
 class ProductView(View):
 
-    def get(self, request):
-        return render(request, 'shop/product-single.html')
+    def get(self, request, product_id=1):
+        data = Product.objects.get(id=product_id)
+        return render(request, 'shop/product-single.html', {"product": data})
 
 
 class ShopView(View):
@@ -20,11 +22,18 @@ class ShopView(View):
             output_field=DecimalField(max_digits=10, decimal_places=2)
         )
 
-        products = Product.objects.annotate(
+        products = Product.objects.select_related('category').annotate(
             discount_value=Subquery(
                 Discount.objects.filter(product_id=OuterRef('id')).values('value')
             ),
             price_before=F('price'),
-            price_after=price_with_discount
-        ).values('id', 'name', 'image', 'price_before', 'price_after', 'discount_value')
-        return render(request, 'shop/shop.html', {"data": products})
+            price_after=price_with_discount,
+        ).values('id', 'name', 'image', 'category', 'price_before', 'price_after', 'discount_value')
+
+        category = request.GET.get("category", "All")
+        if not category == "All":
+            products = products.filter(category__name=category)
+
+        return render(request, 'shop/shop.html',
+                      {"data": products,
+                       "category": category})
