88e9faad9ff78b9549661719b8517418e5ca257c
Egor <egorobidin@inbox.ru>
2023-05-08 19:18:55 +0300

    Работает вывод в корзину

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Cart
+from django.db.models import F
 
 
 class ViewCart(View):
    def get(self, request):
-       return render(request, 'cart/cart.html')
+       cart = Cart.objects.filter(user=request.user).annotate(
+           total_price=F('product__price') * F('quantity')
+       )
+       return render(request, 'cart/cart.html', {"data": cart})
 
 
 class ViewWishlist(View):
