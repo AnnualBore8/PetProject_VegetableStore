5d0ae08ab95e7b79d28e62405dd505db0cbc29c0
Egor <egorobidin@inbox.ru>
2023-05-14 22:29:54 +0300

    Login авторизирует, но не регистрирует

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginView, SingUpView
+from .views import LoginView, SingUpView, LogoutView
 
 app_name = "login"
 
 urlpatterns = [
    path('', LoginView.as_view(), name="login"),
    path('singup/', SingUpView.as_view(), name="singup"),
+   path('logout/', LogoutView.as_view(), name="logout"),
 ]
--- apps/login/views.py
+++ apps/login/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 class LoginView(View):
 
     def get(self, request):
         return render(request, 'login/login.html')
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home:index')
+        return render(request, 'login/login.html', {"error": form.errors.get("__all__")})
+
+
+class LogoutView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+        return redirect('home:index')
 
 
 class SingUpView(View):
